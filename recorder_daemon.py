25362aa2c2e01f787dd3971a92ca5c1fad880b78
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2019-05-12 01:28:58 +0200

    recorder update

--- recorder_daemon.py
+++ recorder_daemon.py
@@ -11,7 +11,7 @@ sock = socket.socket(socket.AF_INET, # Internet
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
 while True:
-    file = open('raw2.txt','a')
+    file = open('recordings/raw2.txt','a')
     
     packet = sock.recvfrom(65535)
 
