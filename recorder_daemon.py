be7a2ac7af0c44553717eeb6902ec002915b1625
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2019-05-11 20:40:20 +0200

    update to the recorder

--- recorder_daemon.py
+++ recorder_daemon.py
@@ -1,4 +1,5 @@
 import socket
+import pandas as pd
 
 UDP_IP = "188.166.115.7"
 UDP_PORT = 7001
@@ -10,9 +11,14 @@ sock = socket.socket(socket.AF_INET, # Internet
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
 while True:
-    file = open('raw.txt','a')
+    file = open('raw2.txt','a')
+    
     packet = sock.recvfrom(65535)
+
     raw_request = str(packet[0])[2:-1]
-    file.write(raw_request+"\n")
+
+    file.write(str(pd.Timestamp.utcnow())+"\t"+raw_request+"\n") #now with timestamp smh im dumb
+    
     print(raw_request)
+
     file.close()
