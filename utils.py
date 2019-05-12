049fd91041b49060c43503b929f24ae2e016c98a
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2019-05-12 05:16:34 +0200

    added non blocking json reader

---
+++ utils.py
@@ -0,0 +1,16 @@
+import json
+
+def read_json(filename):
+    success=False
+
+    while not success:
+        try:
+            with open(filename, "r") as f:
+                raw_json = f.readlines()
+                # print(raw_json)/
+                # print(raw_json[0])
+            json_dict = json.loads(raw_json[0])
+            success=True
+        except IndexError:
+            pass
+    return json_dict
